e6a0704c8910fdf5747c891fbbe13f79e6bbc834
Sacristan <girts.kesteris@gmail.com>
2018-11-12 23:49:40 +0200

    move camera capture to fragment

--- md1/app/src/main/java/com/teamtwo/md1/MainActivity.java
+++ md1/app/src/main/java/com/teamtwo/md1/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupViewPager(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
         adapter.addFragment(new CameraFragment(), "Camera");
-        adapter.addFragment(new CameraFragment(), "Audio");
+        adapter.addFragment(new AudioFragment(), "Audio");
         viewPager.setAdapter(adapter);
     }
 
