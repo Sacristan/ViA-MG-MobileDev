d1f5e0477f0f34050c94d01f81d62ba192657d6b
Sacristan <girts.kesteris@gmail.com>
2018-11-17 16:14:40 +0200

    fix camera setup issue

--- md1/app/src/main/java/com/teamtwo/md1/CameraFragment.java
+++ md1/app/src/main/java/com/teamtwo/md1/CameraFragment.java
@@ -1,6 +1,7 @@
 package com.teamtwo.md1;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CameraFragment extends Fragment {
     private static final String TAG = "CameraFragment";
@@ -19,4 +21,39 @@ public class CameraFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
         return view;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Button captureButton = (Button) findViewById(R.id.button_capture);
+
+//        getCameraInstance().takePicture()
+
+
+        //        GridView gridview = findViewById(R.id.gridview);
+//        gridview.setAdapter(adapter);
+//
+//        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(MainActivity.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
 }
